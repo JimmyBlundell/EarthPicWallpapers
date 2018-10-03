ca32092daa91b75ec561f58e1d4a213ff05bf128
Jimmy Blundell <jblundell123@gmail.com>
2018-10-02 21:28:43 -0500

    Very first, very basic fiddling with praw

---
+++ EarthNudes.py
@@ -0,0 +1,17 @@
+import praw
+
+reddit = praw.Reddit(client_id = 'zpLjVh3uoEoWog',
+                     client_secret = 'j0cyR5uUR6zvGIDmSxSyO6-T9Ao',
+                     username = 'theSuppleLemur',
+                     password = '7mmRem.mag',
+                     user_agent = 'EarthNudesv1')
+
+subreddit = reddit.subreddit('EarthPorn')
+
+hot_python = subreddit.hot(limit = 5)
+
+for submission in hot_python:
+    if not submission.stickied:
+        print(submission.title)
+
+                      
