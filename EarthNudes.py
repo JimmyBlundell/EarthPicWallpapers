deba97cb0d0df852c28c58adc9784d1aaf52001f
Jimmy Blundell <jblundell123@gmail.com>
2018-10-05 20:21:29 -0500

    Stuff

--- EarthNudes.py
+++ EarthNudes.py
@@ -1,10 +1,10 @@
 import praw
 
-reddit = praw.Reddit(client_id = 'zpLjVh3uoEoWog',
-                     client_secret = 'j0cyR5uUR6zvGIDmSxSyO6-T9Ao',
-                     username = 'theSuppleLemur',
-                     password = '7mmRem.mag',
-                     user_agent = 'EarthNudesv1')
+reddit = praw.Reddit(client_id = '---',
+                     client_secret = '---',
+                     username = '---',
+                     password = '---',
+                     user_agent = '---')
 
 subreddit = reddit.subreddit('EarthPorn')
 
@@ -13,6 +13,7 @@ hot_pics = subreddit.hot(limit = 1)
 for submission in hot_pics:
     if not submission.stickied:
         print(submission.title)
+
  
     
                       
