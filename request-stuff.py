45477bf836be689f484fd3feda446c8bc8752de6
Jimmy Blundell <jblundell123@gmail.com>
2018-10-18 16:40:29 -0500

    Figured how to return index where my substring was found. Important

--- request-stuff.py
+++ request-stuff.py
@@ -1,8 +1,11 @@
 import requests
 import json
 url = 'https://www.reddit.com/r/earthporn/hot.json?limit=1'
-r = requests.get(url)
-data = json.loads(r.text)
-target = "sticked"
-for target in data:
-    print('Found it')
+headers = {"User-Agent" : "jimmyboi"}
+r = requests.get(url, headers = headers)
+x = r.text
+print(x)
+#data = json.loads(r.text)
+target = '"stickied": false, "url": "'
+print(x.find(target))
+#this returns the index where I first find the target!! Use this!
